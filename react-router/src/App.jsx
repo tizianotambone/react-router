2e4b6deb2139333684dd743f2a84c5c9cd667fbd
tizianotambone <tambonetiziano@gmail.com>
2025-05-15 14:20:26 +0200

    first commit

---
+++ react-router/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Homepage from './pages/Homepage'
+import About from './pages/About'
+import Products from './pages/Products'
+import {browserRouter, routes,route}from "react-router-dom"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
